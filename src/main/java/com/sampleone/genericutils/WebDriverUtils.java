35e88544f77772f702bf96a1b9eb79f842f30338
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-09-28 10:46:58 +0530

    3 rd commit

---
+++ src/main/java/com/sampleone/genericutils/WebDriverUtils.java
@@ -0,0 +1,12 @@
+package com.sampleone.genericutils;
+
+import org.testng.annotations.Test;
+
+public class WebDriverUtils {
+
+	@Test
+	public void maximizeBrowser()
+	{
+		
+	}
+}
---
+++ src/test/java/com/sampleone/testscripts/TestClass.java
@@ -0,0 +1,12 @@
+package com.sampleone.testscripts;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void testClass1()
+	{
+		System.out.println("hi");
+	}
+}
