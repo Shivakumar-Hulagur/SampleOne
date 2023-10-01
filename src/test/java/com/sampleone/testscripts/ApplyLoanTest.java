beda784065591392c5a477d7350f2b9e4a5bb6f6
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-10-01 10:09:12 +0530

    group.xml commit

---
+++ src/test/java/com/sampleone/testscripts/ApplyLoanTest.java
@@ -0,0 +1,12 @@
+package com.sampleone.testscripts;
+
+import org.testng.annotations.Test;
+
+public class ApplyLoanTest {
+
+	@Test(groups = "smoke")
+	public void applyLoan()
+	{
+		System.out.println(" --Apply loan--");
+	}
+}
