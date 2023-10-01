f4df41008755a314814e08de31021ad9a71b9930
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-10-01 12:59:58 +0530

    Browser test commit

---
+++ src/test/java/com/sampleone/testscripts/BrowserTest.java
@@ -0,0 +1,21 @@
+package com.sampleone.testscripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BrowserTest {
+
+	@Test
+	public void browserTest1() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://localhost:8080/login");
+		driver.findElement(By.id("j_username")).sendKeys("admin");
+		System.out.println(" From browser test1");
+	}
+}
